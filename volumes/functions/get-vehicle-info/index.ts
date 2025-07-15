9f89a32926faaefa9c8e2ec5b525f1e6ac0081bd
Aurelmax <aurelien@gestionmax.fr>
2025-07-15 20:46:26 +0200

    Ultra-minimal version de la fonction Edge pour test de déploiement

--- volumes/functions/get-vehicle-info/index.ts
+++ volumes/functions/get-vehicle-info/index.ts
@@ -1,17 +1,6 @@
-// mod.ts
-// Version ultra-minimale sans import
-
+// Ultra minimal Deno Edge Function
 Deno.serve(async (req) => {
-  return new Response(
-    JSON.stringify({
-      message: "Hello from minimal Edge Function",
-      date: new Date().toISOString()
-    }),
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
-      }
-    }
-  );
+  return new Response(JSON.stringify({ msg: "Hello from Edge Function!" }), {
+    headers: { "Content-Type": "application/json" },
+  })
 });
