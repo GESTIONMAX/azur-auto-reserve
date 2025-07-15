025f20e2b79ded94acb01e996283cacc34d722db
Aurelmax <aurelien@gestionmax.fr>
2025-07-15 21:07:45 +0200

    Mise à jour de la fonction Edge avec la syntaxe exacte de la documentation Supabase

--- volumes/functions/get-vehicle-info/index.ts
+++ volumes/functions/get-vehicle-info/index.ts
@@ -1,4 +1,13 @@
-// Version absolument minimaliste
-export default async function handler(req) {
-  return new Response(JSON.stringify({ message: "Hello World" }));
-}
+// Suivant exactement la documentation Supabase Edge Functions
+import { serve } from "https://deno.land/std@0.131.0/http/server.ts"
+
+serve(async (req) => {
+  const data = {
+    message: "Hello from Supabase Edge Functions!"
+  }
+
+  return new Response(
+    JSON.stringify(data),
+    { headers: { "Content-Type": "application/json" } },
+  )
+})
